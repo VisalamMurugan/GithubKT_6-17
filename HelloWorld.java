baeea1a15eb092ed1a4e9eea2bae47105ff0bce7
VisalamMurugan <visalam.murugan@gmail.com>
2022-06-17 12:45:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Welocme to Github learning");
-		System.out.println("ThQ);
+		System.out.println("ThQ");
 	}
 }
