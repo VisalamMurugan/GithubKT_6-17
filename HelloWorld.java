8197c2bf43ea80548e9d94f8df2eb1e9a71ad163
VisalamMurugan <visalam.murugan@gmail.com>
2022-06-17 12:40:21 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
+		System.out.println("Hi!");
+		System.out.println("thq");
 		System.out.println("Hello World!");
 		System.out.println("thank you");
 		system.out.println("Its a sample pgm')
